842bd38a15e19ea6b4200b033f366df6736aea92
Diego Torres Bertomeu <dtorber@upv.edu.es>
2023-11-20 17:26:05 +0100

    Millora eficiencia Maze

--- Leet Code 75/1926. Nearest Exit from Entrance in Maze/Solution.cpp
+++ Leet Code 75/1926. Nearest Exit from Entrance in Maze/Solution.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        queue<vector<int>> pendents;
+        queue<pair<int, int>> pendents;
         int n = maze.size(), m = maze[0].size(), i, j;
         int distancia[n][m];
-        vector<int> actual;
-
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) 
-                distancia[i][j] = INT_MAX;
-        }
+        pair<int, int> actual;
 
         distancia[entrance[0]][entrance[1]] = 0;
-        pendents.push(entrance);
+        pendents.push({entrance[0], entrance[1]});
 
         while (pendents.size() > 0) {
             actual = pendents.front();
             pendents.pop();
-            i = actual[0];
-            j = actual[1];
-
+            i = actual.first;
+            j = actual.second;
             //si ja es un borde i no te mur hem arribat a l'eixida mes propera
-            if ((i == n - 1 || j == m - 1 || i == 0 || j == 0) && maze[i][j] == '.' && i != entrance[0] && j != entrance[1]) return distancia[i][j];
+            if ((i == n - 1 || j == m - 1 || i == 0 || j == 0) && !(i == entrance[0] && j == entrance[1])) return distancia[i][j];
 
-            if (i + 1 < n && maze[i + 1][j] == '.' && distancia[i + 1][j] > distancia[i][j] + 1) {
-                vector<int> aux;
-                aux.push_back(i + 1);
-                aux.push_back(j);
-                pendents.push(aux);
+            if (i + 1 < n && maze[i + 1][j] == '.') {
+                pendents.push({i + 1, j});
+                maze[i + 1][j] = '+';
                 distancia[i + 1][j] = distancia[i][j] + 1;
             }
-            if (i - 1 >= 0 && maze[i - 1][j] == '.' && distancia[i - 1][j] > distancia[i][j] + 1) {
-                vector<int> aux;
-                aux.push_back(i - 1);
-                aux.push_back(j);
-                pendents.push(aux);
+            if (i > 0 && maze[i - 1][j] == '.') {
+                pendents.push({i - 1, j});
+                maze[i - 1][j] = '+';
                 distancia[i - 1][j] = distancia[i][j] + 1;
             }
-            if (i + 1 < n && maze[i][j + 1] == '.' && distancia[i][j + 1] > distancia[i][j] + 1) {
-                vector<int> aux;
-                aux.push_back(i);
-                aux.push_back(j + 1);
-                pendents.push(aux);
+            if (j + 1 < m && maze[i][j + 1] == '.') {
+                pendents.push({i, j + 1});
+                maze[i][j + 1] = '+';
                 distancia[i][j + 1] = distancia[i][j] + 1;
             }
-            if (i + 1 < n && maze[i][j - 1] == '.' && distancia[i][j - 1] > distancia[i][j] + 1) {
-                vector<int> aux;
-                aux.push_back(i);
-                aux.push_back(j - 1);
-                pendents.push(aux);
+            if (j > 0 && maze[i][j - 1] == '.') {
+                pendents.push({i, j - 1});
+                maze[i][j - 1] = '+';
                 distancia[i][j - 1] = distancia[i][j] + 1;
             }
         }
